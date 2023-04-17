126dcf3338dcc19628d4d09ec23a7881bf994df3
jungtop <jungtop@jungtops-MacBook-Pro.local>
2023-04-17 09:48:43 +0530

    updated create work

--- create_work.py
+++ create_work.py
@@ -1,6 +1,10 @@
 from main import create_work,dump_yaml
 from pathlib import Path
 import csv
+import logging
+
+logging.basicConfig(filename='err.log', filemode='w', format='%(message)s', level=logging.INFO)
+
 
 WORKS_DIR = "new_works"
 
@@ -18,8 +22,11 @@ def main():
    for work_ids in get_work_ids():
         op_work_id,bdrc_work_id = work_ids
         print(bdrc_work_id)
-        work = create_work(op_work_id,op_work_id)
-        dump_yaml(work,Path(f"{WORKS_DIR}/{work['id']}.yml"))
+        try:
+            work = create_work(op_work_id,op_work_id)
+            dump_yaml(work,Path(f"{WORKS_DIR}/{work['id']}.yml"))
+        except Exception as e:
+            logging.info(f"{bdrc_work_id},{e}")
 
 
 if __name__ == "__main__":
